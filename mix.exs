1f85641cc6a5cd95ff1a731bd984841bbd4d986c
Anton Sidorov <anton@ubernil.com>
2019-05-21 20:06:49 +0700

    deps: remove poison

--- mix.exs
+++ mix.exs
@@ -16,7 +16,6 @@ defmodule LoggerJSONFileBackend.Mixfile do
 
   defp deps do
     [
-      {:poison, "~> 1.5 or ~> 2.0"},
       {:uuid, "~> 1.1"},
       {:json, "~> 0.3.2", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev},
